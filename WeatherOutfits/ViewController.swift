54f29f9270554e041927c47ac2e2e1c500b9c00d
LydiaKim <lydiakim@LydiaKims-Mac.local>
2017-02-25 22:12:49 -0800

    Initial Commit

---
+++ WeatherOutfits/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherOutfits
+//
+//  Created by LydiaKim on 2017-02-25.
+//  Copyright © 2017 LydiaKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
